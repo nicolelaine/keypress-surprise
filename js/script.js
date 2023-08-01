2213c900acf54493dca541413eba850dc71cb168
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2023-08-01 19:25:55 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var box1 = document.querySelector(".box-1");
+
+var box2 = document.querySelector(".box-2");
+
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function(e) {
+     var key = e.key;
+      if (e.key === "1") {
+        box1.innerText = "😸";
+      } 
+      else if (e.key === "2") {
+        box2.innerText = "🌭";
+      } 
+      else if (e.key === "3" ) {
+        box3.innerText = "🦴"
+      }
+});
+
+var clear = document.querySelector("button");
+  clear.addEventListener("click", function () {
+      box1.innerText = "1";
+      box2.innerText = "2";
+      box3.innerText = "3";
+  });
